205e5b94475ebf3ffa45e2f0fe5b43fe440d52f7
jrciii <josephcooperiii@gmail.com>
2017-11-15 17:48:14 -0500

    Improve error handling in practice

--- src/MoneyMachine/Practice.hs
+++ src/MoneyMachine/Practice.hs
@@ -1,14 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module MoneyMachine.Practice where
-
 import           Control.Concurrent
-import           Control.Exception
+import           Control.Exception.Lifted
 import           Control.Lens
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.State.Strict
+import Control.Monad.Trans.Control
 import qualified Data.HashMap.Lazy                as HM
 import qualified Data.List                        as L
 import           Data.Maybe
@@ -28,6 +29,7 @@ import           MoneyMachine.Strategy
 import           MoneyMachine.Trade
 import           OANDA
 import           Text.Printf
+import Control.Concurrent.Async
 {-# ANN module ("HLint: ignore Redundant bracket"::String) #-}
 
 practice :: OandaEnv -> AccountID -> CandlestickGranularity -> [String] -> Strategy -> IO ()
@@ -40,54 +42,59 @@ practice env aid granularity instruments strat =
       tzti = TI.hoursToTimeZone (0)
   in evalStateT (forever (go env aid granularity instruments strat granSecs windowSecs windowSize tz tzti)) lastCandle
 
+
+tryAnyIO :: IO a -> IO (Either SomeException a)
+tryAnyIO action = withAsync action waitCatch
+
+tryAny :: MonadBaseControl IO m => m a -> m (Either SomeException a)
+tryAny action =
+    liftBaseWith (\runInIO -> tryAnyIO (runInIO action)) >>=
+    either (return . Left) (liftM Right . restoreM)
+
+catchAny :: MonadBaseControl IO m => m a -> (SomeException -> m a) -> m a
+catchAny action onE = tryAny action >>= either onE return
+
 --go :: (Monad m) => OandaEnv -> AccountID -> CandlestickGranularity -> [String] -> Strategy -> Int -> Int -> THTC.TimeZone -> TI.TimeZone -> StateT (Maybe Candle) m ()
-go env aid granularity instruments strat granSecs windowSecs windowSize tz tzti = do
+go env aid granularity instruments strat granSecs windowSecs windowSize tz tzti = (flip catchAny) (\e -> liftIO $ putStrLn $ "Got error: " ++ show e) $ do
     accountDetails <- makeOandaRequest $ oandaAccountDetails env aid
     let account = accountDetailsResponseAccount accountDetails
     let positions = accountOpenPositionCount account
     if positions > 0
-      then do
-        liftIO $ putStrLn "Open positions, sleeping"
-        liftIO $ threadDelay $ 30 * 1000000
-      else do
-        to <-liftIO $ THC.getCurrentTime
-        let toTime = THTC.addUTCTime (THC.fromSeconds (-3)) to
-        let fromTime = THTC.addUTCTime windowSecs toTime
-        let toZon = THTC.utcToZonedTime tz toTime
-        let fromZon = THTC.utcToZonedTime tz fromTime
-        let args = map (\i -> makeTestArgs (InstrumentName $ T.pack i) granularity fromZon toZon) instruments
-        let reqs = map (makeOandaRequest . oandaCandles env) args
-        resps <- lift $ catchAny (sequence reqs) $ \e -> do
-          putStrLn $ "Got an exception: " ++ show e
-          return []
-        case resps of
-          [] ->  return ()
-          _ -> do
-            lastTime <- get
-            let thisTime = candlestickTime $ head $ candlestickResponseCandles $ head $ resps
-            case lastTime of
-              Just t | (t == thisTime) -> do
-                lift $ putStrLn "Same candle as last"
-                lift $ threadDelay $ 30 * 1000000
-              _ -> do
-                       let candles = map ((map oandaCandleToMMCandle) . candlestickResponseCandles) resps
-                       let sorted = L.sortBy (\a b-> (_candleTime . head $ a) `compare` (_candleTime . head $ b )) candles
-                       let startSec = floor $ TIP.utcTimeToPOSIXSeconds $ TI.localTimeToUTC tzti $ _candleTime $ head $ head sorted
-                       let endSec = floor $ TIP.utcTimeToPOSIXSeconds $ TI.localTimeToUTC tzti $ _candleTime $ last $ head sorted
-                       let filled = map (fillGaps startSec endSec granSecs) candles
-                       let window = HM.fromList (zip instruments (map (\x -> (x,last x)) filled))
-                       --let fromLoc = (THTC.fromThyme $ THTC.utcToLocalTime tz fromTime)
-                       let toLoc = (THTC.fromThyme $ THTC.utcToLocalTime tz toTime)
-                       orders <- lift $ (_onTick strat) window (MO.Spread (MO.Points 0.00015)) (MO.Commission 0.0) [] []
-                       lift $ mapM_ (print . length) filled
-                       lift $ putStrLn $ show orders
-                       let reqs = map (\o -> (makeOandaRequest $ (oandaCreateOrder env aid) $ createOrderDetails o)) orders
-                       resps <- sequence reqs
-                       lift $ putStrLn $ show resps
-                       lift $ threadDelay $ 30 * 1000000
-
-catchAny :: IO a -> (SomeException -> IO a) -> IO a
-catchAny = Control.Exception.catch
+    then do
+      liftIO $ putStrLn "Open positions, sleeping"
+      liftIO $ threadDelay $ 30 * 1000000
+    else do
+      to <-liftIO $ THC.getCurrentTime
+      let toTime = THTC.addUTCTime (THC.fromSeconds (-3)) to
+      let fromTime = THTC.addUTCTime windowSecs toTime
+      let toZon = THTC.utcToZonedTime tz toTime
+      let fromZon = THTC.utcToZonedTime tz fromTime
+      let args = map (\i -> makeTestArgs (InstrumentName $ T.pack i) granularity fromZon toZon) instruments
+      let reqs = map (makeOandaRequest . oandaCandles env) args
+      resps <- sequence reqs
+      lastTime <- get
+      let thisTime = candlestickTime $ head $ candlestickResponseCandles $ head $ resps
+      case lastTime of
+        Just t | (t == thisTime) -> do
+          liftIO $ putStrLn "Same candle as last"
+          liftIO $ threadDelay $ 30 * 1000000
+        _ -> do
+          put $ Just thisTime
+          let candles = map ((map oandaCandleToMMCandle) . candlestickResponseCandles) resps
+          let sorted = L.sortBy (\a b-> (_candleTime . head $ a) `compare` (_candleTime . head $ b )) candles
+          let startSec = floor $ TIP.utcTimeToPOSIXSeconds $ TI.localTimeToUTC tzti $ _candleTime $ head $ head sorted
+          let endSec = floor $ TIP.utcTimeToPOSIXSeconds $ TI.localTimeToUTC tzti $ _candleTime $ last $ head sorted
+          let filled = map (fillGaps startSec endSec granSecs) candles
+          let window = HM.fromList (zip instruments (map (\x -> (x,last x)) filled))
+          --let fromLoc = (THTC.fromThyme $ THTC.utcToLocalTime tz fromTime)
+          let toLoc = (THTC.fromThyme $ THTC.utcToLocalTime tz toTime)
+          orders <- liftIO $ (_onTick strat) window (MO.Spread (MO.Points 0.00015)) (MO.Commission 0.0) [] []
+          liftIO $ mapM_ (print . length) filled
+          liftIO $ print orders
+          let reqs = map (makeOandaRequest . (oandaCreateOrder env aid) . createOrderDetails) orders
+          resps <- sequence reqs
+          liftIO $ print resps
+          liftIO $ threadDelay $ 30 * 1000000
 
 {-placeOrders :: OandaEnv -> [Order] -> IO ()
 placeOrders env [] = return ()
